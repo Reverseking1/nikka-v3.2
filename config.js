60c68305c6e72dd285b527a8143a040ad47b5532
Reverseking1 <127144970+Reverseking1@users.noreply.github.com>
2024-10-25 20:34:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -14,11 +14,11 @@ global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
 global.github = process.env.GITHUB || "https://github.com/hakisolos/Nikka-v3";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaoLotu42DchJmXKBN3L";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://files.catbox.moe/bh2fpj.jpg";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vao11A529758tTEQZo1e";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vao11A529758tTEQZo1e";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.ibb.co/QXbR0M0/IMG-2996.jpg";
 global.devs = "2348121373516";
-global.sudo = process.env.SUDO || "2349112171078";
+global.sudo = process.env.SUDO || "233257514504";
 global.owner = process.env.OWNER_NUMBER || "233257514504";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "true";
@@ -48,11 +48,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ",",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`NIKKA™`",
-  author: process.env.PACK_AUTHER || "HAKI",
+  caption: process.env.CAPTION || "`REVERSE™`",
+  author: process.env.PACK_AUTHER || "KING",
   packname: process.env.PACK_NAME || "KING",
-  botname: process.env.BOT_NAME || "NIKKA-V3",
-  ownername: process.env.OWNER_NAME || "HAKI",
+  botname: process.env.BOT_NAME || "ReverseKing",
+  ownername: process.env.OWNER_NAME || "Reverse King",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
